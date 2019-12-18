0f91f889660a9dadafa525a85068f9b6faa2115f
HannahHa <ashley.han1991@gmail.com>
2019-12-18 02:10:52 +0000

    update

--- load_data.py
+++ load_data.py
@@ -4,11 +4,11 @@ import pymongo
 CRIME_SOURCE = "data.lacity.org"
 client = pymongo.MongoClient()
 
-def load(start_date = '2015-01-01T00:00:00.000'):
+def load(start_date):
     results = download_crime(url=CRIME_SOURCE, start_date = start_date)
     db = client.get_database("crime")
     collection = db.get_collection("crime")
-    collection.insertMany(results)
+    collection.insert_many(results)
         
 if __name__ == '__main__':
-    load(start_date = '2010-01-01T00:00:00.000')
+    load(start_date = '2015-01-01T00:00:00.000')
