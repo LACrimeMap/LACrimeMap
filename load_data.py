02b4cb101da81dd17bfbeaf0bca1a18d621a67c2
HannahHa <ashley.han1991@gmail.com>
2019-12-18 01:35:57 +0000

    5 year

--- load_data.py
+++ load_data.py
@@ -4,7 +4,7 @@ import pymongo
 CRIME_SOURCE = "data.lacity.org"
 client = pymongo.MongoClient()
 
-def load(start_date = '2010-01-01T00:00:00.000'):
+def load(start_date = '2015-01-01T00:00:00.000'):
     results = download_crime(url=CRIME_SOURCE, start_date = start_date)
     db = client.get_database("crime")
     collection = db.get_collection("crime")
