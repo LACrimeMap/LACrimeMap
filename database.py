1321abc7832b487967f4f31817602da70360a8b4
HannahHa <ashley.han1991@gmail.com>
2019-12-20 20:04:57 +0000

    account for empty upsert

--- database.py
+++ database.py
@@ -13,18 +13,19 @@ RESULT_CACHE_EXPIRATION = 2200
 
 def upsert_crime(df):
     """
-    Update MongoDB database `energy` and collection `energy` with the given `DataFrame`.
+    Update MongoDB database `crime` and collection `crime` with the given `DataFrame`.
     """
     db = client.get_database("crime")
     collection = db.get_collection("crime")
     update_count = 0
-    for record in df.to_dict('records'):
-        result = collection.replace_one(
-            filter = {'rpt_id': record['rpt_id']},    # locate the document if exists
-            replacement = record,                         # latest document
-            upsert=True)
-        if result.matched_count > 0:
-            update_count += 1
+    if len(df) > 0:
+        for record in df.to_dict('records'):
+            result = collection.replace_one(
+                filter = {'rpt_id': record['rpt_id']},    # locate the document if exists
+                replacement = record,                         # latest document
+                upsert=True)
+            if result.matched_count > 0:
+                update_count += 1
     logger.info("rows={}, update={}, ".format(df.shape[0], update_count) +
                 "insert={}".format(df.shape[0]-update_count))
 
@@ -40,9 +41,9 @@ _fetch_all_crime_as_df_cache = expiringdict.ExpiringDict(max_len=1,
                                                        max_age_seconds=RESULT_CACHE_EXPIRATION)
 
 def fetch_all_crime_as_df(allow_cached=False):
-    """Converts list of dicts returned by `fetch_all_bpa` to DataFrame with ID removed
+    """Converts list of dicts returned by `fetch_all_crime` to DataFrame with ID removed
     Actual job is done in `_worker`. When `allow_cached`, attempt to retrieve timed cached from
-    `_fetch_all_bpa_as_df_cache`; ignore cache and call `_work` if cache expires or `allow_cached`
+    `_fetch_all_crime_as_df_cache`; ignore cache and call `_work` if cache expires or `allow_cached`
     is False.
     """
     def _work():
